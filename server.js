b6b6e4052678cc1fa098faa9b8942fd1f2b60544
thewifiproject <pierrejacksoo@seznam.cz>
2024-12-20 21:01:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const chalk = require('chalk');
 
 const app = express();
 const port = 3000;
+const localIP = '10.0.1.33'; // Change this to your local IP address
 
 // Middleware to parse incoming JSON or URL-encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,7 +23,7 @@ app.post('/', (req, res) => {
     res.send('Credentials received');
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// Start the server and bind it to the local IP address
+app.listen(port, localIP, () => {
+    console.log(`Server is running on http://${localIP}:${port}`);
 });
