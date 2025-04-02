1d8972729ece9627634d77ca9b51093ac40fc7dc
thewifiproject <pierrejacksoo@seznam.cz>
2025-04-02 13:53:42 +0200

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,53 +1,54 @@
 import argparse
-from scapy.all import rdpcap, Dot11, EAPOL
+from scapy.all import *
+from scapy.layers.dot11 import Dot11, Dot11Auth, Dot11AssoReq, Dot11ReassocReq, Dot11Elt
 
 def extract_info(pcap_file):
+    # Read the pcap file
     packets = rdpcap(pcap_file)
-    ssid = ""
-    bssid = ""
-    ap_mac = ""
-    sta_mac = ""
-    key_mic = ""
-    anonce = ""
-    snonce = ""
+    
+    key_mic = None
+    anonce = None
+    snonce = None
+    ssid = None
+    bssid = None
+    ap_mac = None
+    sta_mac = None
 
-    for pkt in packets:
-        if pkt.haslayer(Dot11):
-            if pkt.type == 0 and pkt.subtype == 8:  # Beacon frame
-                ssid = pkt.info.decode()
-                bssid = pkt.addr2
-                ap_mac = pkt.addr2
+    for packet in packets:
+        if packet.haslayer(Dot11):
+            # Extract the SSID and BSSID from Beacon frames or Probe responses
+            if packet.type == 0 and packet.subtype == 8:  # Beacon frame
+                ssid = packet[Dot11Elt].info.decode() if packet.haslayer(Dot11Elt) else None
+                bssid = packet[Dot11].addr3
 
-            if pkt.type == 0 and pkt.subtype == 4:  # Probe request
-                sta_mac = pkt.addr2
+            # Look for Message 2 and Message 3 (RSN info for WPA handshake)
+            if packet.haslayer(Dot11Auth):
+                if packet[Dot11Auth].algorithm == 0:  # Open System Auth (for WPA handshake)
+                    if packet[Dot11Auth].seqnum == 2:  # Message 2
+                        snonce = packet[Dot11Auth].nonce
+                        sta_mac = packet[Dot11].addr2
+                        ap_mac = packet[Dot11].addr1
+                    elif packet[Dot11Auth].seqnum == 3:  # Message 3
+                        anonce = packet[Dot11Auth].nonce
+                        key_mic = packet[Dot11Auth].keymic
 
-        if pkt.haslayer(EAPOL):
-            eapol_pkt = pkt[EAPOL]
-            if eapol_pkt.type == 3:  # Key message
-                key_data = eapol_pkt.load
-                if len(key_data) > 0:
-                    key_mic = key_data[-16:]
-                    if not anonce:
-                        anonce = key_data[13:45]
-                    if not snonce:
-                        snonce = key_data[45:77]
-
-    return ssid, bssid, ap_mac, sta_mac, key_mic, anonce, snonce
+    return key_mic, anonce, snonce, ssid, bssid, ap_mac, sta_mac
 
 def main():
-    parser = argparse.ArgumentParser(description='Extract WiFi handshake information from a pcap file.')
-    parser.add_argument('pcap_file', help='The path to the pcap or .cap file')
+    parser = argparse.ArgumentParser(description='Extract WPA handshake information from a pcap or .cap file')
+    parser.add_argument('file', help='Path to the pcap or .cap file')
     args = parser.parse_args()
 
-    ssid, bssid, ap_mac, sta_mac, key_mic, anonce, snonce = extract_info(args.pcap_file)
-    
-    print(f'SSID: {ssid}')
-    print(f'BSSID: {bssid}')
-    print(f'AP MAC: {ap_mac}')
-    print(f'STA MAC: {sta_mac}')
-    print(f'Key MIC: {key_mic.hex()}')
-    print(f'ANonce: {anonce.hex()}')
-    print(f'SNonce: {snonce.hex()}')
+    key_mic, anonce, snonce, ssid, bssid, ap_mac, sta_mac = extract_info(args.file)
 
-if __name__ == '__main__':
+    # Print out the extracted information
+    print("Key MIC: ", key_mic)
+    print("ANonce: ", anonce)
+    print("SNonce: ", snonce)
+    print("SSID: ", ssid)
+    print("BSSID: ", bssid)
+    print("AP MAC: ", ap_mac)
+    print("STA MAC (Client MAC): ", sta_mac)
+
+if __name__ == "__main__":
     main()
