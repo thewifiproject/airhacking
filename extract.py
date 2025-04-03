15f75ccd4e0f6b6ba5b447b7272e0ca4ffc85db0
thewifiproject <pierrejacksoo@seznam.cz>
2025-04-03 13:26:15 +0200

    Update extract.py

--- extract.py
+++ extract.py
@@ -10,7 +10,7 @@ def extract_mic_and_nonce(input_file):
     for packet in packets:
         if packet.haslayer(EAPOL):
             eapol_layer = packet.getlayer(EAPOL)
-            # Check if it's a Key (Message 2 of 4)
+            # Check if it's a Key (Message 2 of 4) or Message 3 of 4
             if eapol_layer.type == 3 and eapol_layer.key_mic:
                 mic = eapol_layer.key_mic
                 print(f"Extracted MIC: {mic.hex()}")
@@ -18,15 +18,13 @@ def extract_mic_and_nonce(input_file):
                     snonce = eapol_layer.key_nonce
                     print(f"Extracted SNonce: {snonce.hex()}")
                 return mic.hex()
-
             # Check if it's a Key (Message 3 of 4)
-            if eapol_layer.type == 3 and not eapol_layer.key_mic:
+            elif eapol_layer.type == 3 and not eapol_layer.key_mic:
                 anonce = eapol_layer.key_nonce
                 print(f"Extracted ANonce: {anonce.hex()}")
 
     if not snonce:
         print("No SNonce found in Message 2 of 4.")
-    
     if not anonce:
         print("No ANonce found in Message 3 of 4.")
 
