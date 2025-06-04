7df80d3aa4557a0a6f000e3023d1cb95516c7a09
thewifiproject <pierrejacksoo@seznam.cz>
2025-06-04 14:23:33 +0200

    Update amx.py

--- amx.py
+++ amx.py
@@ -12,6 +12,14 @@ import threading
 import concurrent.futures
 import time
 from tqdm import tqdm
+import signal
+import sys
+
+def signal_handler(sig, frame):
+    print("\n[!] Přerušeno uživatelem (CTRL+C), ukončuji...")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 # Multiprocessing imports
 import multiprocessing
